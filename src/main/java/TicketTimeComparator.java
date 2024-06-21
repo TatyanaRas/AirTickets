054ab3f85aba76d5f3fca38d84c0c602e9ddada3
Tatyana <tatyanalipch@yandex.ru>
2024-06-21 22:46:31 +0300

    src/

--- src/main/java/TicketTimeComparator.java
+++ src/main/java/TicketTimeComparator.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class TicketTimeComparator {
+import java.util.Comparator;
+
+//import static jdk.vm.ci.sparc.SPARC.o1;
+//import static jdk.vm.ci.sparc.SPARC.o2;
+
+public class TicketTimeComparator implements Comparator<Ticket> {
+
+    @Override
+    public int compare(Ticket o1, Ticket o2) {
+        // ваш код
+        int time1 = o1.getTimeTo() - o1.getTimeFrom();
+        int time2 = o2.getTimeTo() - o2.getTimeFrom();
+
+        if (time1 < time2) {
+            return -1;
+        } else if (time1 > time2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
 }
--- src/test/java/AviaSoulsTest.java
+++ src/test/java/AviaSoulsTest.java
@@ -1,2 +1,165 @@
-package PACKAGE_NAME;public class AviaSoulsTest {
+//import jdk.internal.jimage.ImageStrings;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Stack;
+
+public class AviaSoulsTest {
+
+    @Test
+
+    public void priceСomparison1() {
+        AviaSouls ticket = new AviaSouls();
+
+        Ticket ticket1 = new Ticket("Москва", "Париж", 60000, 8, 24);
+        Ticket ticket2 = new Ticket("Москва", "Париж", 30000, 8, 18);
+        Ticket ticket3 = new Ticket("Москва", "Париж", 43000, 10, 16);
+        Ticket ticket4 = new Ticket("Москва", "Воронеж", 10000, 8, 12);
+        Ticket ticket5 = new Ticket("Москва", "Воронеж", 7000, 9, 14);
+
+        ticket.add(ticket1);
+        ticket.add(ticket2);
+        ticket.add(ticket3);
+        ticket.add(ticket4);
+        ticket.add(ticket5);
+
+        Ticket[] actual = ticket.search("Москва", "Париж");
+        Ticket[] expected = {ticket2, ticket3, ticket1};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+
+    public void priceСomparison2() {
+        AviaSouls ticket = new AviaSouls();
+
+        Ticket ticket1 = new Ticket("Москва", "Генуя", 20000, 8, 24);
+        Ticket ticket2 = new Ticket("Москва", "Париж", 20000, 8, 18);
+        Ticket ticket3 = new Ticket("Москва", "Париж", 20000, 10, 16);
+        Ticket ticket4 = new Ticket("Москва", "Воронеж", 20000, 8, 12);
+        Ticket ticket5 = new Ticket("Москва", "Воронеж", 20000, 9, 14);
+
+        ticket.add(ticket1);
+        ticket.add(ticket2);
+        ticket.add(ticket3);
+        ticket.add(ticket4);
+        ticket.add(ticket5);
+
+        Ticket[] actual = ticket.search("Москва", "Генуя");
+        Ticket[] expected = {ticket1};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+    public void priceСomparison3() {
+        AviaSouls ticket = new AviaSouls();
+
+        Ticket ticket1 = new Ticket("Москва", "Париж", 20000, 8, 24);
+        Ticket ticket2 = new Ticket("Москва", "Париж", 30000, 8, 18);
+        Ticket ticket3 = new Ticket("Москва", "Париж", 43000, 10, 16);
+        Ticket ticket4 = new Ticket("Москва", "Воронеж", 10000, 8, 12);
+        Ticket ticket5 = new Ticket("Москва", "Воронеж", 7000, 9, 14);
+
+        ticket.add(ticket1);
+        ticket.add(ticket2);
+        ticket.add(ticket3);
+        ticket.add(ticket4);
+        ticket.add(ticket5);
+
+        Ticket[] actual = ticket.search("Москва", "Волгоград");
+        Ticket[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+    }
+
+    @Test
+// сортировка по времени перелета
+    public void sortTicket() {
+        AviaSouls ticket = new AviaSouls();
+        Comparator<Ticket> comporator = new TicketTimeComparator();
+
+        Ticket ticket1 = new Ticket("Москва", "Париж", 20000, 8, 24);//24-8=16
+        Ticket ticket2 = new Ticket("Москва", "Париж", 30000, 8, 18);//18-8=10
+        Ticket ticket3 = new Ticket("Москва", "Париж", 43000, 10, 16);//16-10=6
+        Ticket ticket4 = new Ticket("Москва", "Воронеж", 10000, 8, 12);
+        Ticket ticket5 = new Ticket("Москва", "Воронеж", 7000, 9, 14);
+
+        ticket.add(ticket1);
+        ticket.add(ticket2);
+        ticket.add(ticket3);
+        ticket.add(ticket4);
+        ticket.add(ticket5);
+
+        Ticket[] actual = ticket.search("Москва", "Париж", comporator);
+        Ticket[] expected = {ticket3, ticket2, ticket1};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+
+    public void sortTicketSameTime() {//одинаковое время
+        AviaSouls ticket = new AviaSouls();
+        Comparator<Ticket> comporator = new TicketTimeComparator();
+
+        Ticket ticket1 = new Ticket("Москва", "Генуя", 20000, 8, 24);
+        Ticket ticket2 = new Ticket("Москва", "Париж", 30000, 8, 18);
+        Ticket ticket3 = new Ticket("Москва", "Париж", 43000, 10, 16);
+        Ticket ticket4 = new Ticket("Москва", "Воронеж", 10000, 8, 12);//4
+        Ticket ticket5 = new Ticket("Москва", "Воронеж", 7000, 9, 13);//4
+
+        ticket.add(ticket1);
+        ticket.add(ticket2);
+        ticket.add(ticket3);
+        ticket.add(ticket4);
+        ticket.add(ticket5);
+
+
+        Ticket[] actual = ticket.search("Москва", "Генуя", comporator);
+        Ticket[] expected = {ticket1};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
+    @Test
+
+    public void sortTicketSameTime2() {
+        AviaSouls ticket = new AviaSouls();
+        Comparator<Ticket> comporator = new TicketTimeComparator();
+
+        Ticket ticket1 = new Ticket("Москва", "Генуя", 20000, 8, 24);
+        Ticket ticket2 = new Ticket("Москва", "Париж", 30000, 8, 18);
+        Ticket ticket3 = new Ticket("Москва", "Париж", 43000, 10, 16);
+        Ticket ticket4 = new Ticket("Москва", "Воронеж", 10000, 8, 12);
+        Ticket ticket5 = new Ticket("Москва", "Воронеж", 7000, 9, 13);
+
+        ticket.add(ticket1);
+        ticket.add(ticket2);
+        ticket.add(ticket3);
+        ticket.add(ticket4);
+        ticket.add(ticket5);
+
+
+        Ticket[] actual = ticket.search("Москва", "Волгоград", comporator);
+        Ticket[] expected = {};
+
+        Assertions.assertArrayEquals(expected, actual);
+
+
+    }
+
 }
+
+
